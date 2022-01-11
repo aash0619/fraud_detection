f6dfc1e66cc243f775f290f4ca703149ff131d8b
aash0619 <aashpreet06@gmail.com>
2022-01-11 19:16:19 +0530

    choosing 3rd model as best model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("this is all code relatd to ML")
+
+def model1():
+    print('this is model 1')
+
+def model2():
+    print('this is model 2')
+
+def model3():
+    print('this is model 3')
+
+
+print('model 3 is the best pick')
